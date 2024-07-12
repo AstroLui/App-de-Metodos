3e9875b73db6a63aad41807e99fdaba24f600ab3
MoisesLondo <moiseslondo16@gmail.com>
2024-07-12 19:28:02 -0400

    filas y columnas ficticias

--- app/solTrasporte.py
+++ app/solTrasporte.py
@@ -19,6 +19,21 @@ def Enter_Demanda_Oferta(n, m):
     return oferta, demanda
         
 def Start(matriz, oferta, demanda, n, m, modo):
+
+    totalOferta = sum(oferta)
+    totalDemanda = sum(demanda)
+    if totalOferta != totalDemanda:
+        if totalOferta>totalDemanda:
+            for fila in matriz:
+                fila.append(0)
+            m += 1
+            valorFaltante = totalOferta-totalDemanda
+            demanda.append(valorFaltante)
+        else:
+            matriz.append([0 for _ in range(m + 1)])
+            n += 1
+            valorFaltante = totalDemanda-totalOferta
+            oferta.append(valorFaltante)
     mode = pulp.const.LpMinimize if(modo == "min") else pulp.const.LpMaximize
     prob = pulp.LpProblem("Problema de Trasnporte", mode)
     variables = pulp.LpVariable.dicts("Rutas",[(i,j) for i in range(n) for j in range(m)], lowBound=0)
